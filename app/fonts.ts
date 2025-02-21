ba67ec9d6e471323c844c2ebe69663b575b810ec
dibkb <dibas9110@gmail.com>
2025-02-21 16:37:37 +0530

    first commit

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Geist_Mono, Manrope } from "next/font/google";
+
+export const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+export const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
