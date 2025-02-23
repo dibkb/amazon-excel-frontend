0cefc6b1490dff47bbf8367f0f2339c143378d5e
dibkb <dibas9110@gmail.com>
2025-02-24 00:56:39 +0530

    specs

--- app/_components/amazon/product-information/product-row.tsx
+++ app/_components/amazon/product-information/product-row.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+const bannedKeys = ["Customer Reviews"];
 const ProductRow = ({ row, value }: { row: string; value: string }) => {
+  if (bannedKeys.includes(row)) return null;
+  let valueContent;
+  if (row === "Best Sellers Rank") {
+    valueContent = (
+      <span className="flex flex-col gap-1">
+        {value
+          .split("#")
+          .slice(1)
+          .map((ele) => {
+            return (
+              <p key={ele} className="font-semibold text-blue-700">
+                #{ele}
+              </p>
+            );
+          })}
+      </span>
+    );
+  } else {
+    valueContent = <p className="font-medium">{value}</p>;
+  }
   return (
     <div className="grid grid-cols-2 gap-1 border-b p-2 text-sm">
       <p className="font-bold">{row}</p>
-      <p className="font-medium">{value}</p>
+      {valueContent}
     </div>
   );
 };
