f8a886ee4f071da46048a55f7c739b4e325ad0a5
dibkb <dibas9110@gmail.com>
2025-02-26 05:55:10 +0530

    unique products

--- app/_components/amazon/tabs/swot.tsx
+++ app/_components/amazon/tabs/swot.tsx
@@ -1,14 +1,26 @@
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 
 import SelectedProducts from "../accordion/swot/selected-products";
 import { productStore } from "@/app/store/productStore";
-
+import { Competitor } from "@/src/api/models/Competitor";
 const Sowt = () => {
   const { product } = productStore();
+  const relatedProducts = useMemo(() => {
+    const set = new Set();
+    const relatedProducts = product?.product.related_products;
+    const uniqueRelatedProducts: Competitor[] = [];
+    relatedProducts?.forEach((product) => {
+      if (!set.has(product.asin)) {
+        set.add(product.asin);
+        uniqueRelatedProducts.push(product);
+      }
+    });
+    return uniqueRelatedProducts;
+  }, [product]);
   return (
     <div className="mt-6">
-      <SelectedProducts relatedProducts={product?.product.related_products} />
+      <SelectedProducts relatedProducts={relatedProducts} />
     </div>
   );
 };
