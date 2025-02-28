5fa22ff1285d7ffe3b5a3c020836ba1263ff31bc
dibkb <dibas9110@gmail.com>
2025-02-28 20:49:27 +0530

    signinpage

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import Eye from "@/svg/Eye";
 import EyeSlash from "@/svg/eyeslash";
+import Github from "@/svg/github";
+import Google from "@/svg/google";
+import Link from "next/link";
 
 import React, { useState } from "react";
 
@@ -63,6 +67,33 @@ const SignInPage = () => {
             Remember me
           </Label>
         </div>
+
+        <Button className="bg-blue-500 hover:bg-blue-600 text-white py-6 px-4 rounded-md text-sm font-bold">
+          Sign in
+        </Button>
+        <div className="flex flex-col gap-2 border-t border-stone-200 pt-8 mt-4">
+          <Button className="bg-stone-700 hover:bg-stone-800 text-white py-6 px-4 rounded-md text-sm font-bold">
+            <Google />
+            Or sign in with Google
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-2 mt-2">
+          <Button className="bg-stone-50 hover:bg-stone-100 border text-stone-500 py-6 px-4 rounded-md text-sm font-bold">
+            <Github />
+            Or sign in with Github
+          </Button>
+        </div>
+
+        <p className="text-xs text-stone-500 text-center mt-3">
+          Don&apos;t have an account?{""}
+          <Link
+            href="/auth/signup"
+            className="text-blue-500 hover:text-blue-600 font-bold ml-1"
+          >
+            Sign up now
+          </Link>
+        </p>
       </form>
     </div>
   );
